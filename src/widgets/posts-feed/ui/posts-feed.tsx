8d41a82835c4cb410a3450936de72df39f8b68bc
Luka Khimshiashvili <lukakhimshiashvili4@gmail.com>
2025-10-05 13:09:12 +0400

    gix: show onnly herocard

--- src/widgets/posts-feed/ui/posts-feed.tsx
+++ src/widgets/posts-feed/ui/posts-feed.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { PostCard } from "@/entities/post/ui";
 import HeroPostCard from "@/entities/post/ui/post-card/heroUI/hero-post-card";
 import { usePosts } from "@/shared/hooks/use-posts";
 import { Post } from "@/shared/types";
@@ -18,9 +17,13 @@ export default function PostsFeed({ posts, postCardType }: PostsFeedProps) {
 
   return (
     <div className="mt-8 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      {postCardType === 0
+      {/* {postCardType === 0
         ? data?.data?.map((post) => <PostCard key={post.id} post={post} />)
-        : data?.data?.map((post) => <HeroPostCard key={post.id} post={post} />)}
+        : data?.data?.map((post) => <HeroPostCard key={post.id} post={post} />)} 
+        */}
+      {data?.data?.map((post) => (
+        <HeroPostCard key={post.id} post={post} />
+      ))}
     </div>
   );
 }
